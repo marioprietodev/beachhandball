3f4adcf8070ebab60941621f19bcaa5190740b40
marioprietodev <marioprietodev@gmail.com>
2026-05-18 17:25:39 +0200

    Entity Torneo

--- src/main/java/com/beachhandball/beachhandball/entity/Torneo.java
+++ src/main/java/com/beachhandball/beachhandball/entity/Torneo.java
@@ -1,8 +1,28 @@
 package com.beachhandball.beachhandball.entity;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
 
 @Entity
+@Data
+@Table(name = "torneo")
 public class Torneo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    @NotBlank
+    private String nombre;
+    @Column (nullable = false)
+    @DateTimeFormat (pattern = "yyyy-MM-dd'T'HH:mm")
+    private LocalDateTime fecha;
+    @Column ( nullable = false)
+    @NotBlank
+    private String lugar;
+
 
 }
