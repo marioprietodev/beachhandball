30152bf959404472d8749bd6a80333355f3eeab3
marioprietodev <marioprietodev@gmail.com>
2026-05-18 23:22:07 +0200

    Operaciones CRUD Parte 2 Torneo

--- src/main/java/com/beachhandball/beachhandball/controller/TorneoController.java
+++ src/main/java/com/beachhandball/beachhandball/controller/TorneoController.java
@@ -3,20 +3,19 @@ package com.beachhandball.beachhandball.controller;
 import com.beachhandball.beachhandball.entity.Torneo;
 import com.beachhandball.beachhandball.repository.TorneoRepository;
 import com.beachhandball.beachhandball.service.TorneoService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 public class TorneoController {
 private final TorneoService torneoService;
+private final TorneoRepository torneoRepository;
 
 
-    public TorneoController(TorneoService torneoService) {
+    public TorneoController(TorneoService torneoService, TorneoRepository torneoRepository) {
         this.torneoService = torneoService;
+        this.torneoRepository = torneoRepository;
     }
     @GetMapping("/torneos")
     public List<Torneo> listarTorneo(){
@@ -25,5 +24,17 @@ private final TorneoService torneoService;
     @PostMapping ("/torneos/crear")
     public Torneo crearTorneo(@RequestBody Torneo torneo){
         return torneoService.crearTorneo(torneo);
+    }
+    @GetMapping ("/torneos/{id}")
+    public Torneo buscarIdTorneo(@PathVariable Long id){
+        return torneoService.buscarIdTorneo(id);
+    }
+    @DeleteMapping ("/torneos/eliminar/{id}")
+    public void eliminar (@PathVariable Long id){
+        if (!torneoRepository.existsById(id)){
+            System.out.println("Torneo no encontrado");
+        }
+        torneoService.eliminar(id);
+
     }
 }
